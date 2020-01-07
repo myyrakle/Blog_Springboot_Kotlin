375b4e07f770abc72a7a671ab3b02f66cb527c0f
myyrakle <sssang97@naver.com>
2020-01-07 09:38:08 +0900

    commit

--- src/main/kotlin/org/myyrakle/myblog/controller/PostController.kt
+++ src/main/kotlin/org/myyrakle/myblog/controller/PostController.kt
@@ -1,21 +1,16 @@
 package org.myyrakle.myblog.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class PostController
 {
-    @RequestMapping(value=["/do_login"], method= [RequestMethod.POST])
-    fun doLogin(): String
+    @RequestMapping(value=["/post/{id}"])
+    fun viewSinglePost(@PathVariable id:Long): String
     {
-        return "redirect:/"
-    }
-
-    @RequestMapping(value=["/do_logout"], method= [RequestMethod.POST])
-    fun doLogout(): String
-    {
-        return "redirect:/"
+        return "";
     }
 }
